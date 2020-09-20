248e08f76f86defee700485c15289b356e78a76f
Silver Saul <saul@on24.ee>
2020-09-21 02:26:41 +0300

    Add CountingValleys solution

---
+++ HackerRank/Warm-up Challenges/CountingValleys.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        int valleyCounter = 0;
+        int height = 0;
+        boolean isInValley = false;
+
+        for (char step : path.toCharArray()) {
+            if (step == 'D') {
+                height--;
+
+                if (height == -1) {
+                    isInValley = true;
+                }
+            } else {
+                height++;
+
+                if (height == 0) {
+                    isInValley = false;
+                    valleyCounter++;
+                }
+            }
+        }
+
+        return valleyCounter;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
