21c325f14361b23a44b6191efc44cd672c48ec1c
Silver Saul <saul@on24.ee>
2020-10-15 10:23:53 +0300

    Add 2DArray Solution

---
+++ HackerRank/Arrays/2DArrayDS.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class 2DArrayDS {
+
+    static int hourglassSum(int[][] arr) {
+        int biggestSum = Integer.MIN_VALUE;
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                biggestSum = Math.max(getHourGlassValueAtLocation(arr, y, x), biggestSum);
+            }
+        }
+
+        return biggestSum;
+
+    }
+
+    static int getHourGlassValueAtLocation(int[][] arr, int y, int x) {
+        return arr[y][x]     + arr[y][x + 1]     + arr[y][x + 2]
+                             + arr[y + 1][x + 1]
+             + arr[y + 2][x] + arr[y + 2][x + 1] + arr[y + 2][x + 2];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
